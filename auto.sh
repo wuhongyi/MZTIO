bebef90ea98320fe6bef260100b873b12d4e0c4d
wuhongyi <wuhongyi@qq.com>
2019-09-24 21:14:45 +0800

    en/zh README

--- auto.sh
+++ auto.sh
@@ -2,7 +2,9 @@
 
 module load texlive/2019
 
-rm -rf docs/*
+rm -rf docs/doctrees/*
+rm -rf docs/en/*
+rm -rf docs/zh/*
 cp index.html docs/
 touch docs/.nojekyll
 
@@ -11,7 +13,7 @@ cd en/
 make clean
 make html
 make latexpdf
-mv build/doctrees/ ../docs/
+cp -r build/doctrees/* ../docs/doctrees/
 cp -r build/html/* ../docs/en/
 cp build/latex/mztio.pdf ../README_en.pdf
 make clean
@@ -21,7 +23,7 @@ cd zh/
 make clean
 make html
 make latexpdf
-cp -r build/html/* ../../docs/zh/
+cp -r build/html/* ../docs/zh/
 cp build/latex/mztio.pdf ../README_zh.pdf
 make clean
 cd ../
---
+++ docs/zh/_sources/index.rst.txt
@@ -0,0 +1,25 @@
+.. MZTIO documentation master file, created by
+   sphinx-quickstart on Fri Jul 12 22:18:04 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to MZTIO's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+=================================
+
+.. toctree::
+   :maxdepth: 3
+   :caption: GUIDE
+
+   README.md
+   webgui.rst
+   RemoteControl.md
+   ubuntu.md
+   Vivado.md
+   experiment.md
+   code.md
